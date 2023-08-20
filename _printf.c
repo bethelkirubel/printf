9b2744291be2079c9ed70868475cc20fcd546aab
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-20 07:12:11 -0700

    third commit

--- _printf.c
+++ _printf.c
@@ -9,50 +9,54 @@
 int _printf(const char *format, ...)
 {
 int i = 0;
-va_list list;
-int len;
-char c;
+va_list val;
+int len, len2;
 char *s;
+char c;
 
 if (format == NULL)
 return (-1);
-
-va_start(list, format);
+va_start(val, format);
 while (*format)
 {
 if (*format != '%')
 {
-write (1, format, 1);
+write(1, format, 1);
 i++;
 }
 else
 {
 format++;
-
-if(*format == '\0')
+if (*format == '\0')
 break;
-
-if(*format == '%')
-write(1, format, 1);
+len2 = 0;
+if (*format == '%')
+{
+_putchar('%');
 i++;
-
-if(*format == 'c')
-c = va_arg(list, int);
+i = i + len2;
+}
+if (*format == 'c')
+{
+c = va_arg(val, int);
 write(1, &c, 1);
 i++;
-
-if(*format == 's')
-s = va_arg(list, char *);
+}
+if (*format == 's')
+{
+s = va_arg(val, char *);
 len = 0;
-while(s[len] != '\0')
+while (s[len] != '\0')
 {
 len++;
 }
 write(1, s, len);
 i = i + len;
 }
+}
 format++;
-va_end(list);
 }
-return(i);
+va_end(val);
+return (i);
 }
+
